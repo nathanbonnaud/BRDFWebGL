395e2700698b5e26fab5b1e8f7f28d12eb6b3493
Dulieu Maxime <maxime.dulieu@etu.unilim.fr>
2020-10-17 18:27:09 +0200

    nettoyage final

--- Projet1/WebGLCanevas2020/shaders/obj.fs
+++ Projet1/WebGLCanevas2020/shaders/obj.fs
@@ -21,30 +21,30 @@ const float M_PI = 3.145;
 
 // =======================================================================================================
 
-// Calcule l'aspect diffus du fragment à l'aide du modèle de Lambert
+// Modèle de Lambert
 vec3 Lambert(vec3 Lpos, vec3 Objcolor, vec3 N, vec4 Pos3D, float RhoD)
 {
-	vec3 L = vec3(Lpos-vec3(Pos3D));
-	float cosT = max(dot(N,normalize(L)),0.0);
-	vec3 Kd = RhoD * Objcolor * cosT;
+    vec3 L = vec3(Lpos-vec3(Pos3D));
+    float cosT = max(dot(N,normalize(L)),0.0);
+    vec3 Kd = RhoD * Objcolor * cosT;
 
-	return Kd ;
+    return Kd ;
 }
 
 // ======================================================================================================
 
-// Calcule le modèle de Phong en fusionnant l'aspect diffus et spéculaire
+// Modèle Blinn-Phong modifié
 float BlinnPhongModify(vec3 Lpos, vec3 N, vec4 Pos3D , float n)
 {
 	float Ks = (1.0-uRhoD);
 
 	vec3 L = vec3(vec3(Pos3D)-Lpos);
 	vec3 CamDir = -vec3(Pos3D);
-  vec3 Lreflect = reflect(normalize(L), N);
-  float cosA = pow(max(dot(Lreflect, normalize(CamDir)), 0.0), n);
+    vec3 Lreflect = reflect(normalize(L), N);
+    float cosA = pow(max(dot(Lreflect, normalize(CamDir)), 0.0), n);
 	float Is = Ks * ((n+8.0)/(8.0*M_PI)) * cosA;
 
-  return Is;
+    return Is;
 }
 
 //============================================================================================================
@@ -52,10 +52,10 @@ float BlinnPhongModify(vec3 Lpos, vec3 N, vec4 Pos3D , float n)
 //Fresnel
 float F(vec3 L, vec3 M, float Ni)
 {
-  float c = max(dot(L,M),0.0);
+    float c = max(dot(L,M),0.0);
 	float Ni2 = Ni*Ni;
 	float c2 = c*c;
-  float g = sqrt(Ni2+c2-1.0);
+    float g = sqrt(Ni2+c2-1.0);
 	float gmc = g-c;
 	float gpc = g+c;
 	float gmc2 = gmc*gmc;
@@ -64,27 +64,27 @@ float F(vec3 L, vec3 M, float Ni)
 	float top = (c*gpc -1.0)*(c*gpc-1.0);
 	float bot = (c*gmc +1.0)*(c*gmc+1.0);
 	float coeff1 = gmc2/(2.0*gpc2);
-  float coeff2 = 1.0 + top/bot;
+    float coeff2 = 1.0 + top/bot;
 	float F = coeff1*coeff2;
 
-  return F;
+    return F;
 }
 
 //================================================================================================================
 
-//coefficient d'ombrage
+// Maskage / Ombrage
 float G(vec3 N, vec3 M, vec3 L, vec3 CamDir)
 {
-  float val1 = (2.0*max(dot(N,M),0.0)*max(dot(N,CamDir),0.0)/max(dot(CamDir,M),0.0));
-  float val2 = (2.0*max(dot(N,M),0.0)*max(dot(N,L),0.0)/max(dot(L,M),0.0));
+    float val1 = (2.0*max(dot(N,M),0.0)*max(dot(N,CamDir),0.0)/max(dot(CamDir,M),0.0));
+    float val2 = (2.0*max(dot(N,M),0.0)*max(dot(N,L),0.0)/max(dot(L,M),0.0));
 	float G = min(1.0,min(val1,val2));
 
-  return G;
+    return G;
 }
 
 //=================================================================================================================
 
-//calcul de la distribution de Beckmann
+// Distribution des normales de Beckmann
 float D(vec3 N, vec3 M, float Sigma)
 {
 	float CosT = max(dot(N,M),0.0);
@@ -94,17 +94,17 @@ float D(vec3 N, vec3 M, float Sigma)
 	float TanT2 = SinT2/CosT2;
 	float Sigma2 = Sigma*Sigma;
 
-  float e = exp(-TanT2/(2.0*Sigma2));
-  float bot = M_PI*Sigma2*CosT4;
+    float e = exp(-TanT2/(2.0*Sigma2));
+    float bot = M_PI*Sigma2*CosT4;
 	float D = e/bot;
 
-  return D;
+    return D;
 }
 
 
 //========================================================================================================
 
-//somme des couleurs et retourne la couleur finale de l'objet
+// BRDF de Cook-Torrance
 vec3 CookTorrance(vec3 Lpos, vec3 Objcolor, vec3 N, vec4 Pos3D)
 {
     vec3 L = normalize(vec3(Lpos-vec3(Pos3D)));
@@ -126,21 +126,21 @@ vec3 CookTorrance(vec3 Lpos, vec3 Objcolor, vec3 N, vec4 Pos3D)
 
 //=========================================================================================================
 
-// Fonction principale du fragement shader, il renvoit la couleur final de chaque fragment.
 void main(void)
 {
 	vec3 Fr;
 	vec3 Li = uLi*uLcolor;
 	float CosT = max(dot(normalize(N),normalize(uLpos-vec3(pos3D))),0.0);
 
-  if(uTorranceOn == 0)
+    if(uTorranceOn == 0)
 	{
-		// Modèle Blinn Phong modifié
+		// Modèle Blinn-Phong modifié
 		vec3 Kd = Lambert(uLpos,uObjcolor,normalize(N),pos3D, uRhoD);
 		Fr = Kd/M_PI + BlinnPhongModify(uLpos,normalize(N),pos3D,uN);
-	}else
+	}
+	else
 	{
-		// Modèle Cook Torrance
+		// Modèle Cook-Torrance
 		Fr = CookTorrance(uLpos,uObjcolor,normalize(N),pos3D);
 	}
 
